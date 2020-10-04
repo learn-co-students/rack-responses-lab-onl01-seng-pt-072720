c2b42032a8618025f7d767640869ba8537973541
fentyhall <fentymhall@gmail.com>
2020-10-04 15:40:26 -0700

    Done.

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
 
         if Time.now.hour < 12
             resp.write "Good Morning!"
-        else Time.now.hour >= 12
+        else 
             resp.write "Good Afternoon!"
         end 
 
